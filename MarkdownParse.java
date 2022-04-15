84d861c998934dbe9ab4ce74f408602a29dda512
JeffreyLi <47624796+jeffreyli640@users.noreply.github.com>
2022-04-15 14:21:28 -0700

    Bug Fix
    Fixed the bug where empty brackets would cause an exception to thrown.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,12 +11,16 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while(currentIndex < markdown.length() && currentIndex >= 0) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(openParen >= 0 && closeParen > openParen) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            } else {
+                break;
+            }
             currentIndex = closeParen + 1;
         }
 
@@ -28,6 +32,6 @@ public class MarkdownParse {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+	    System.out.println(links.toString());
     }
 }
